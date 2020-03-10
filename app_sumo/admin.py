4594d67ba5460f6e87b5e7b15c033d5706d3de31
YujiIkeda <ikeda.yuji@kyodonews.jp>
2020-03-10 19:01:30 +0900

    model.py、admin.pyからテスト用に作成していたテーブルを削除

--- app_sumo/admin.py
+++ app_sumo/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Eventinfo, Player, Waza, Outcome, Match, PostCsv, Mst_Rikishi, Mst_Rikishistatus, Mst_Heya, Mst_Basho, Mst_Kimarite, Mst_Award_category, Mst_Hometown, Mst_Class, Mst_Chii, Mst_Event, Mst_Nichime, Mst_Eastwest, Mst_Lifetime_result, Mst_Lifetime_award, Mst_Lifetime_statusinfo
+from .models import Mst_Rikishi, Mst_Rikishistatus, Mst_Heya, Mst_Basho, Mst_Kimarite, Mst_Award_category, Mst_Hometown, Mst_Class, Mst_Chii, Mst_Event, Mst_Nichime, Mst_Eastwest, Mst_Lifetime_statusinfo, Mst_Lifetime_result, Mst_Lifetime_award
 
 class Mst_ChiiAdmin(admin.ModelAdmin):
     fieldssets = [
@@ -13,14 +13,6 @@ class Mst_ChiiAdmin(admin.ModelAdmin):
     list_display = ('Chii_code', 'Chii_kanji', 'Chii_kana', 'Chii_2char', 'Chii_3char')
 
 
-admin.site.register(Eventinfo)
-admin.site.register(Player)
-admin.site.register(Waza)
-admin.site.register(Outcome)
-admin.site.register(Match)
-admin.site.register(PostCsv)
-
-# ----------------------------------------------------------------------
 admin.site.register(Mst_Rikishi)
 admin.site.register(Mst_Rikishistatus)
 admin.site.register(Mst_Heya)
--- app_sumo/forms.py
+++ app_sumo/forms.py
@@ -1,9 +1,1 @@
 from django import forms
-
-from django.forms import ModelForm
-from .models import Match
-
-class MatchForm(forms.ModelForm):
-    class Meta:
-        model = Match
-        fields = ('player1', 'outcome1', 'waza', 'outcome2', 'player2',)
