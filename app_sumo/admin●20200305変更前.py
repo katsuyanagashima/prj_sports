9adf146eeb54a822a39d3170d31b3e72032894b1
fmrok <fum1t4ke@gmail.com>
2020-03-10 19:21:36 +0900

    勝負情報を追加

--- app_sumo/admin●20200305変更前.py
+++ app_sumo/admin●20200305変更前.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Eventinfo, Player, Waza, Outcome, Match, PostCsv, Mst_Rikishi, Mst_Rikishistatus, Mst_Heya, Mst_Basho, Mst_Kimarite, Mst_Award_category, Mst_Hometown, Mst_Class, Mst_Chii, Mst_Event, Mst_Nichime, Mst_Eastwest, Mst_Lifetime_result, Mst_Lifetime_award, Mst_Lifetime_statusinfo, Mst_Gameinfo
+from .models import Eventinfo, Player, Waza, Outcome, Match, PostCsv, Mst_Rikishi, Mst_Rikishistatus, Mst_Heya, Mst_Basho, Mst_Kimarite, Mst_Award_category, Mst_Hometown, Mst_Class, Mst_Chii, Mst_Event, Mst_Nichime, Mst_Eastwest
+
 
 class Mst_ChiiAdmin(admin.ModelAdmin):
     fieldssets = [
@@ -33,7 +34,5 @@ admin.site.register(Mst_Chii, Mst_ChiiAdmin)
 admin.site.register(Mst_Event)
 admin.site.register(Mst_Nichime)
 admin.site.register(Mst_Eastwest)
-admin.site.register(Mst_Lifetime_statusinfo)
-admin.site.register(Mst_Lifetime_result)
-admin.site.register(Mst_Lifetime_award)
-admin.site.register(Mst_Gameinfo)
+
+
