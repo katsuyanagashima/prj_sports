0667c695913c423b4b9c31d99f27e95051e0e23f
oba2020 <oba.shinnosuke@kyodonews.jp>
2020-03-26 20:20:30 +0900

    oba2020032601

--- app_sumo/forms.py
+++ app_sumo/forms.py
@@ -2,8 +2,16 @@ from django import forms
 
 from django.forms import ModelForm
 from .models import Mst_Heya
+from .models import Mst_Event
 
 class Mst_HeyaForm(forms.ModelForm):
     class Meta:
         model = Mst_Heya
         fields = ('Heya_code', 'Heya_official_kanji', 'Heya_official_kana', 'Heya_kanji_2char', 'Heya_kanji_3char',)
+
+class Mst_Event_Form(forms.ModelForm):
+    
+    class Meta:
+        model = Mst_Event
+        fields = ('Event_date', 'Torikumi_nichime_code', 'Shoubu_nichime_code', 'Basho_code', 'Frist_date', 'Banzuke_date', 'Age_calcu_reference_date')
+
