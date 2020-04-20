35bc913df67f7bf6ee2449e800e12b8a95bc5a1b
YujiIkeda <ikeda.yuji@kyodonews.jp>
2020-04-20 13:35:45 +0900

    UNI_2020-384 オートレース受信処理　機能修正、機能追加 追加

---
+++ app_autorace/consts.py
@@ -0,0 +1,9 @@
+GO_RECURSIVELY = False # 再帰的
+
+SCHEDULEDATA = "scheduleData"
+PROGRAMDATA = "programData"
+RESULTDATA = "resultData"
+TOP30PRIZEDATA = "top30prizeData"
+OUTSIDETRACKDATA = "outsidetrackData"
+
+PAUSED = False
