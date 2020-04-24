875d52d075537211b4bf3660e789f650c2b80e04
YujiIkeda <ikeda.yuji@kyodonews.jp>
2020-04-24 10:04:11 +0900

    UNI_2020-384 オートレース受信処理　機能修正、機能追加

--- app_autorace/management/commands/trn_receiving_autorace.py
+++ app_autorace/management/commands/trn_receiving_autorace.py
@@ -49,81 +49,102 @@ class Command(BaseCommand):
         # parser.add_argument('command_id', nargs='+', type=int)
 
     """受け取った引数を登録する"""
+
+    def run_trn_schedule(self):
+        baseScheduleData = os.path.normpath(os.path.join(base_trn, SCHEDULEDATA))
+        target_dir_scheduleData = os.path.expanduser(baseScheduleData)
+        
+        event_handler_schedule = watchdoc.WatchDocHandler(target_file_schedule_record)
+        logger.info("target_dir_scheduleData:" + target_dir_scheduleData )
+        self.observer_trn_schedule.schedule(event_handler_schedule, target_dir_scheduleData, recursive=GO_RECURSIVELY)# recursive再帰的
+        # 監視実行
+        self.observer_trn_schedule.start()
+        logger.info("scheduleData: スケジュール監視　Start" )
+
+    def run_trn_program(self):
+        baseProgramData = os.path.normpath(os.path.join(base_trn, PROGRAMDATA))
+        target_dir_programData = os.path.expanduser(baseProgramData)
+
+        event_handler_result = watchdoc.WatchDocHandler(target_file_program_record)
+        logger.info("target_dir_programData:" + target_dir_programData )
+        self.observer_trn_program.schedule(event_handler_result, target_dir_programData, recursive=GO_RECURSIVELY)# recursive再帰的
+        # 監視実行
+        logger.info("programData: 番組編成監視　Start")
+        self.observer_trn_program.start()
+
+    def run_trn_result(self):
+        baseResultData = os.path.normpath(os.path.join(base_trn, RESULTDATA))
+        target_dir_resultData = os.path.expanduser(baseResultData)
+
+        event_handler_result = watchdoc.WatchDocHandler(target_file_result_record)
+        logger.info("target_dir_resultData:" + target_dir_resultData )
+        self.observer_trn_result.schedule(event_handler_result, target_dir_resultData, recursive=GO_RECURSIVELY)# recursive再帰的
+        # 監視実行
+        logger.info("resultData: レース結果監視　Start")
+        self.observer_trn_result.start()
+
+    def run_trn_top_30_prize(self):
+        baseTop30prizeData = os.path.normpath(os.path.join(base_trn, TOP30PRIZEDATA))
+        target_dir_top30prizeData = os.path.expanduser(baseTop30prizeData)
+
+        event_handler_result = watchdoc.WatchDocHandler(target_file_top30prize_record)
+        logger.info("target_dir_top30prizeData:" + target_dir_top30prizeData )
+        self.observer_trn_top_30_prize.schedule(event_handler_result, target_dir_top30prizeData, recursive=GO_RECURSIVELY)# recursive再帰的
+        # 監視実行
+        logger.info("top30prizeData: 選手取得賞金上位３０位監視　Start")
+        self.observer_trn_top_30_prize.start()
+
+    def run_trn_outsidetrack(self):
+        baseOutsidetrackData = os.path.normpath(os.path.join(base_trn, OUTSIDETRACKDATA))
+        target_dir_outsidetrackData = os.path.expanduser(baseOutsidetrackData)
+
+        event_handler_result = watchdoc.WatchDocHandler(target_file_outsidetrack_record)
+        logger.info("target_dir_outsidetrackData:" + target_dir_outsidetrackData )
+        self.observer_trn_outsidetrack.schedule(event_handler_result, target_dir_outsidetrackData, recursive=GO_RECURSIVELY)# recursive再帰的
+        # 監視実行
+        logger.info("outsidetrackData: 場外売場情報監視　Start")
+        self.observer_trn_outsidetrack.start()
+
     # ファイル監視の開始
     def handle(self, *args, **options):
 
         # 監視対象ディレクトリを指定する
         # スケジュールレコード（mmddhhmmss00000000.dat） 1: スケジュール
         try:
-            baseScheduleData = os.path.normpath(os.path.join(base_trn, SCHEDULEDATA))
-            target_dir_scheduleData = os.path.expanduser(baseScheduleData)
-            
-            event_handler_schedule = watchdoc.WatchDocHandler(target_file_schedule_record)
-            logger.info("target_dir_scheduleData:" + target_dir_scheduleData )
-            self.observer_trn_schedule.schedule(event_handler_schedule, target_dir_scheduleData, recursive=GO_RECURSIVELY)# recursive再帰的
-            # 監視実行
-            self.observer_trn_schedule.start()
-            logger.info("scheduleData: スケジュール監視　Start" )
+            self.run_trn_schedule()
+
         except Exception as e:
             logger.error(e)
 
         # 監視対象ディレクトリを指定する
         # 番組編成データレコード（mmddhhmmss0000J001.dat） 2: 番組編成
         try:
-            baseProgramData = os.path.normpath(os.path.join(base_trn, PROGRAMDATA))
-            target_dir_programData = os.path.expanduser(baseProgramData)
+            self.run_trn_program()
 
-            event_handler_result = watchdoc.WatchDocHandler(target_file_program_record)
-            logger.info("target_dir_programData:" + target_dir_programData )
-            self.observer_trn_program.schedule(event_handler_result, target_dir_programData, recursive=GO_RECURSIVELY)# recursive再帰的
-            # 監視実行
-            logger.info("programData: 番組編成監視　Start")
-            self.observer_trn_program.start()
         except Exception as e:
             logger.error(e)
 
         # 監視対象ディレクトリを指定する
         # レース結果データレコード（mmddhhmmss0000JRR2.dat） 3: レース結果
         try:
-            baseResultData = os.path.normpath(os.path.join(base_trn, RESULTDATA))
-            target_dir_resultData = os.path.expanduser(baseResultData)
+            self.run_trn_result()
 
-            event_handler_result = watchdoc.WatchDocHandler(target_file_result_record)
-            logger.info("target_dir_resultData:" + target_dir_resultData )
-            self.observer_trn_result.schedule(event_handler_result, target_dir_resultData, recursive=GO_RECURSIVELY)# recursive再帰的
-            # 監視実行
-            logger.info("resultData: レース結果監視　Start")
-            self.observer_trn_result.start()
         except Exception as e:
             logger.error(e)
 
         # 監視対象ディレクトリを指定する
         # 選手取得賞金上位３０位レコード（mmddhhmmss00000003.dat） 4: 選手取得賞金上位３０位
         try:
-            baseTop30prizeData = os.path.normpath(os.path.join(base_trn, TOP30PRIZEDATA))
-            target_dir_top30prizeData = os.path.expanduser(baseTop30prizeData)
+            self.run_trn_top_30_prize()
 
-            event_handler_result = watchdoc.WatchDocHandler(target_file_top30prize_record)
-            logger.info("target_dir_top30prizeData:" + target_dir_top30prizeData )
-            self.observer_trn_top_30_prize.schedule(event_handler_result, target_dir_top30prizeData, recursive=GO_RECURSIVELY)# recursive再帰的
-            # 監視実行
-            logger.info("top30prizeData: 選手取得賞金上位３０位監視　Start")
-            self.observer_trn_top_30_prize.start()
         except Exception as e:
             logger.error(e)    
 
         # 監視対象ディレクトリを指定する
         # 場外売場情報（mmddhhmmss00000004.dat） 5: 場外売場情報 
         try:
-            baseOutsidetrackData = os.path.normpath(os.path.join(base_trn, OUTSIDETRACKDATA))
-            target_dir_outsidetrackData = os.path.expanduser(baseOutsidetrackData)
+            self.run_trn_outsidetrack()
 
-            event_handler_result = watchdoc.WatchDocHandler(target_file_outsidetrack_record)
-            logger.info("target_dir_outsidetrackData:" + target_dir_outsidetrackData )
-            self.observer_trn_outsidetrack.schedule(event_handler_result, target_dir_outsidetrackData, recursive=GO_RECURSIVELY)# recursive再帰的
-            # 監視実行
-            logger.info("outsidetrackData: 場外売場情報監視　Start")
-            self.observer_trn_outsidetrack.start()
         except Exception as e:
             logger.error(e)
 
@@ -149,6 +170,7 @@ class Command(BaseCommand):
                 #    logger.warning("outsidetrackData: 場外売場情報監視　End")                    
                 #    return False
 
+        # except処理
         except KeyboardInterrupt:
             self.observer_trn_schedule.stop()
             self.observer_trn_program.stop()
