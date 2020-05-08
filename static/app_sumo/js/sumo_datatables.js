0db7e752c6c529759c2c5ea74c852e2226a0658d
fmrok <fum1t4ke@gmail.com>
2020-05-08 11:43:34 +0900

    優勝三賞入力の表示崩れを修正

--- static/app_sumo/js/sumo_datatables.js
+++ static/app_sumo/js/sumo_datatables.js
@@ -9,28 +9,30 @@ $(function () {
             paginate: {next: '次', previous: '前',},
             zeroRecords: "データはありません。",
         },
-        order: [[2, 'asc'], [4, 'asc'], [0, 'asc']], // 並び順
+        order: [[7, 'asc'], [8, 'asc'], [6, 'asc']], // 並び順
         columnDefs: [
-            <!-- [0]東西コード -->
-            <!-- [1]東西 -->
-            <!-- [2]階級コード -->
-            <!-- [3]階級 -->
-            <!-- [4]番付順位 -->
-            <!-- [5]力士名 -->
-            <!-- [6] -->
-            {orderable: false, targets: [0, 1, 2, 3, 4, 5, 6]}, // 利用者による並べ替えの禁止
-            {targets: [0, 2], visible: false}, // 非表示の列
-            {targets: 3, width: '110px'},
-            {targets: 5, width: '110px'},
+            //<!-- [0]東西 -->
+            //<!-- [1]階級 -->
+            //<!-- [2]地位 -->
+            //<!-- [3]番付順位 -->
+            //<!-- [4]力士名 -->
+            //<!-- [5]ラジオボタン -->
+            //<!-- [6]東西コード -->
+            //<!-- [7]階級コード -->
+            //<!-- [8]番付順位 -->
+            {orderable: false, targets: [0, 1, 2, 3, 4, 5, 6, 7, 8]}, // 利用者による並べ替えの禁止
+            {targets: [6, 7, 8], visible: false}, // 非表示の列
         ],
+        autowidth: false,
         paginate: false, // ページング
         info: false, // 件数情報
         //lengthChange: false, // 件数切替プルダウン
         //lengthMenu: [ [5, 15, -1], [5, 15, 'All'] ],
+        scrollX: true, // ヘッダーがずれるので使用しない
         scrollY: '40vh', // 縦方向のスクロールバー
         stateSave: true, // 表示条件の保存
         initComplete: function () { // プルダウンで階級を選択
-            this.api().columns([3]).every(function () {
+            this.api().columns([1]).every(function () {
                 var column = this;
                 var select = $('<select><option value="">−−−</option></select>')
                     .appendTo($(column.header()).empty())
@@ -54,17 +56,16 @@ $(function () {
         },
         dom: '<<t>f>\'', // 検索ボックスの位置
     });
+    // ヘッダーとボディの位置ずれを解消
+    $('#tbl_SUMYUS01_banzuke').css('margin-left', '0px');
     // 選択した行の背景色を変更、ラジオボタンにチェック、優勝・三賞の指定ボタンの有効／無効
     $('#tbl_SUMYUS01_banzuke tbody').on('click', 'tr', function () {
         if ($(this).hasClass('selected')) {
             $(this).removeClass('selected');
-            //$(this).children('.selectedRikishi').children('input').prop('checked', false);
             $(this).find('input').prop('checked', false);
         } else {
-            //myTable.$('tr.selected').removeClass('selected');
             $('tr.selected').removeClass('selected');
             $(this).addClass('selected');
-            //$(this).children('.selectedRikishi').children('input').prop('checked', true);
             $(this).find('input').prop('checked', true);
         }
         // ボタンの有効／無効
@@ -87,9 +88,25 @@ $(document).on('init.dt', function (e, settings) {
     var api = new $.fn.dataTable.Api(settings);
     var state = api.state.loaded();
     if (state) {
-        var val_init = state['columns'][3]['search']['search'].replace(/\^|\$/g, '');
+        var val_init = state['columns'][1]['search']['search'].replace(/\^|\$/g, '');
         $('#class_code_menu').val(val_init);
     }
+    //階級別の場合、ボディ部に階級を表示しない
+    if ($('#class_code_menu option:selected').text() == '−−−')
+        $('td.class_code').removeClass('hidden');
+    else
+        $('td.class_code').hide();
+});
+
+//階級別に番付を表示する場合、ボディの各行に階級を表示しない
+$(function () {
+    // セレクトボックスが切り替わったら発動
+    $('select#class_code_menu').change(function () {
+        if ($('#class_code_menu option:selected').text() == '−−−')
+            $('td.class_code').show();
+        else
+            $('td.class_code').hide();
+    });
 });
 
 // 優勝・三賞画面の指定済みテーブル
